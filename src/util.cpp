684f59f40ed94e30d22577ce4672b718f3472461
ratt <ratt@19a12193-7728-0410-8f54-e9f61ebc0c9f>
2002-10-25 05:17:01 +0000

    Exp required update
    git-svn-id: https://svn.code.sf.net/p/seq/svn/showeq/trunk@167 19a12193-7728-0410-8f54-e9f61ebc0c9f

--- src/util.cpp
+++ src/util.cpp
@@ -649,7 +649,11 @@ uint32_t calc_exp (int level, uint16_t race, uint8_t class_)
   else if (level<57)	exp*=23;
   else if (level<58)	exp*=25;
   else if (level<59)	exp*=27;
-  else                exp*=30;
+  else if (level<60)    exp*=30;
+// Rough estimate so far, seems to hold up perfectly though.
+// Kinder, gentler exping?  Less punishing time commitment?  Haha I don't friggin think so.
+// -- Ratt
+  else                exp*=32;
   
   // Do the race mod
   
